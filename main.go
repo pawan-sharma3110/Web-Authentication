e25d91007e69d531ee80b2f16e12177aa2e0d44a
Pawan <pawansharma93520@gmail.com>
2024-07-07 23:11:01 +0530

    json token create

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
+var jwtKey = []byte{}
+
 type UserClaims struct {
 	jwt.StandardClaims
 	SessionID int64
@@ -24,4 +26,12 @@ func (u *UserClaims) Valid() error {
 
 func main() {
 
+}
+func (u *UserClaims) CreateToken() (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, u)
+	tokenStr, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", fmt.Errorf("error in create token when sining token : %w", err)
+	}
+	return tokenStr, nil
 }

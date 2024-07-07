581c181f56b37ee1064d7ce21b00bca5e771139c
Pawan <pawansharma93520@gmail.com>
2024-07-08 02:26:01 +0530

    Authantication using middlware

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module auth
 
 go 1.22.2
 
-require github.com/golang-jwt/jwt/v4 v4.5.0
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	golang.org/x/crypto v0.25.0
+)
+
+require (
+	github.com/google/uuid v1.6.0 //direct
+	github.com/lib/pq v1.10.9 // direct
+)
--- main.go
+++ main.go
@@ -1,37 +1,153 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
 	"time"
 
-	"github.com/golang-jwt/jwt/v4"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
-var jwtKey = []byte{}
+var db *sql.DB
 
-type UserClaims struct {
-	jwt.StandardClaims
-	SessionID int64
-}
-
-func (u *UserClaims) Valid() error {
-	if !u.VerifyExpiresAt(time.Now().Unix(), true) {
-		return fmt.Errorf("token has expired")
-	}
-	if u.SessionID == 0 {
-		return fmt.Errorf("invalid session ID")
-	}
-	return nil
+type User struct {
+	ID       uuid.UUID `json:"id"`
+	Email    string    `json:"email"`
+	Password string    `json:"password"`
 }
 
 func main() {
+	http.HandleFunc("/register", registerUser)
+	http.HandleFunc("/login", loginUser)
+	http.Handle("/protected", jwtMiddleware(http.HandlerFunc(protectedEndpoint)))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
-}
-func (u *UserClaims) CreateToken() (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, u)
-	tokenStr, err := token.SignedString(jwtKey)
+func init() {
+	var err error
+	connStr := "host=localhost port=5432 user=postgres dbname=user sslmode=disable password=Pawan@2003"
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
-		return "", fmt.Errorf("error in create token when sining token : %w", err)
+		log.Fatal(err)
 	}
-	return tokenStr, nil
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
 }
+
+func registerUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user.ID = uuid.New()
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	user.Password = string(hash)
+
+	err = saveUserToDB(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]string{"id": user.ID.String()}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func saveUserToDB(user User) error {
+	query := `INSERT INTO users (id, email, password) VALUES ($1, $2, $3)`
+	_, err := db.Exec(query, user.ID, user.Email, user.Password)
+	return err
+}
+
+func loginUser(w http.ResponseWriter, r *http.Request) {
+	var credentials struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&credentials)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var user User
+	err = db.QueryRow("SELECT id, email, password FROM users WHERE email = $1", credentials.Email).Scan(&user.ID, &user.Email, &user.Password)
+	if err != nil {
+		http.Error(w, "Invalid email or password", http.StatusUnauthorized)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(credentials.Password))
+	if err != nil {
+		http.Error(w, "Invalid email or password", http.StatusUnauthorized)
+		return
+	}
+
+	token, err := generateJWT(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]string{"token": token}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func generateJWT(user User) (string, error) {
+	claims := jwt.MapClaims{
+		"id":    user.ID,
+		"email": user.Email,
+		"exp":   time.Now().Add(time.Minute * 2).Unix(),
+		"iat":   time.Now().Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte("secret"))
+}
+
+func jwtMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := r.Header.Get("Authorization")
+		if tokenString == "" {
+			http.Error(w, "Missing token", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, http.ErrAbortHandler
+			}
+			return []byte("secret"), nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is a protected endpoint!"))
+}
+
+// remember my program and make an api get all, user access all user list after login  use middelware
